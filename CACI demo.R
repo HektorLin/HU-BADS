47876431251a7855f1ef87c02c592aca78dda9c6
HektorLin <hector.chen.lin@gmail.com>
2018-10-25 21:11:53 +0200

    CACI demo

---
+++ CACI demo.R
@@ -0,0 +1,10 @@
+#Practicing CACI demo
+
+#csv and excel in/out control
+setwd("C:/Users/Hector Lin/Documents/HU-courses-practice/CACI")
+ice = read.csv("PersonIceCreamImportance.csv")
+
+#install.packages("xlsx")
+#Sys.setenv(JAVA_HOME='C:/Program Files/Java/jdk-11.0.1')
+library("xlsx")
+
